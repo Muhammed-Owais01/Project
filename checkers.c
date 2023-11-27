7696834b9312aebfa192714ec3c7d43f7613818a
Muhammed-Owais01 <83649329+Muhammed-Owais01@users.noreply.github.com>
2023-11-27 16:22:50 +0500

    Update checkers.c

--- checkers.c
+++ checkers.c
@@ -1,1 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<stdbool.h>
+#define SIZE 8
 
+
+// board initialization
+char board[SIZE][SIZE] =  {{'X', ' ', 'X', ' ','X', ' ', 'X', ' '},
+                          {' ', 'X', ' ', 'X', ' ', 'X', ' ', 'X'},
+                          {'X', ' ', 'X', ' ', 'X', ' ', 'X', ' '},
+                          {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                          {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                          {' ', 'O', ' ', 'O', ' ', 'O', ' ', 'O'},
+                          {'O', ' ', 'O', ' ', 'O', ' ', 'O', ' '},
+                          {' ', 'O', ' ', 'O', ' ', 'O', ' ', 'O'}};
